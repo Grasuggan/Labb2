401fa9b1f99c8a71e3dfe0a770e71fea69dcdf29
Grasuggan <32665572+Grasuggan@users.noreply.github.com>
2021-04-08 16:48:32 +0200

    renderer

--- src/components/testsnap/ContactForm.Snapshots.test.js
+++ src/components/testsnap/ContactForm.Snapshots.test.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Greeting from "../Greeting.js";
 import renderer from "react-test-renderer";
-import submitters from "../../../tools/mockData";
-import ContactForm from "../ContactForm.js"
-import SubmitCounts from "../SubmitCounts.js"
+
 
 it("should match Greet snapshot", () => {
     const greet = renderer.create(  
